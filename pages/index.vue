a384d0e666ad1d5565c00be883102253015947ee
Tao <unitedstatesofsocilistrepublic@gmail.com>
2022-05-19 11:25:15 +0700

    19/5 noon

--- pages/index.vue
+++ pages/index.vue
@@ -184,16 +184,23 @@ export default {
       cdthumbimg: null,
       currentTime: 0,
       vol: 1,
+      track: [],
     }
   },
   created() {
+    while (this.track.length < 20) {
+      var value = Math.floor(Math.random() * 8500) + 1
+      if (this.track.indexOf(value) === -1) {
+        this.track.push(value)
+      }
+    }
     for (var i = 0; i < 20; i++) {
       axios
         .get('https://api2.bcdcnt.net/graphql', {
           params: {
             query:
               'query($id: ID!) {\n song(id: $id) {\nid\ntitle\nsubtitle\nslug\ncontent\nviews\ndownloads\nfile_type\nlyric_type\nstatus\nyear\nrecord_year\nallow_comment\ncreated_at\nartists(first: 10, orderBy: [{column: "song_artist.order", order: ASC}]) {\ndata {\nid\ntitle\nslug\navatar {\nurl\n}\n}\n}\ncomposers(first: 10, orderBy: [{column: "song_composer.order", order: ASC}]) { \n data { \n id\n title\n slug\n }\n }\n poets(first: 10) {\n data {\nid\n  title\n slug\n  }\n }\n thumbnail {\n url\n user {\n id\n username\navatar {\nurl\n}\n}\n}\nfile {\nvideo_url\naudio_url\n }\n uploader {\n id\n username\n avatar {\nurl\n}\n}\ndocuments(first: 10) {\ndata {\nid\ntitle\nslug\nthumbnail {\n url\n}\n}\n}\n}\n}',
-            variables: { id: `${Math.floor(Math.random() * 8500) + 1}` },
+            variables: { id: `${this.track[i]}` },
           },
         })
         .then((res) => {
